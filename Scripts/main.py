f897f6baf32db74ce3ed6736fa00bfdea884d2aa
forzetofficial <leonidremix@gmail.com>
2021-02-21 21:52:04 +0300

    Исправил

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,9 +1,10 @@
 import pygame
+
 import settings
 
 pygame.init()
 
-screen = pygame.display.set_mode(settings.Height, settings.Weight)
-
+screen = pygame.display.set_mode([settings.Weight, settings.Height])
+pygame.display.set_caption("Змейка")
 
 pygame.quit()
